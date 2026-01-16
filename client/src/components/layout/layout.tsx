6a5e9c74a5088bd3ebbbfaa8f8c23540dce703c0
Lasha Andghuladze <l.andghuladze97@gmail.com>
2026-01-17 03:54:38 +0400

    fix: minor fixes for layout on mobile.

--- client/src/components/layout/layout.tsx
+++ client/src/components/layout/layout.tsx
@@ -7,13 +7,14 @@ const Layout = () => {
     <div className='min-h-screen bg-background antialiased'>
       <nav className='border-b border-finch-200 dark:border-finch-800'>
         <div className='container mx-auto flex h-16 items-center px-4'>
-          <div className='mr-4 hidden md:flex justify-between w-full'>
+          <div className='mr-4 flex justify-between w-full'>
             <div className='flex items-center'>
-              <Link to='/' className='mr-6 flex items-center gap-3 space-x-2 group'>
+              <Link to='/' className='mr-6 flex items-center gap-3 space-x-2 group font-bold'>
                 <span className='bg-finch-100 dark:bg-finch-800 transition-colors duration-300 rounded-full p-2 group-hover:bg-finch-300 dark:group-hover:bg-finch-700'>
                   <Bookmark />
                 </span>
-                Task Management Dashboard
+                <span className='hidden md:block'>Task Management Dashboard</span>
+                <span className='block md:hidden'>TMD</span>
               </Link>
               <nav className='flex items-center space-x-6 text-sm font-medium'>
                 <Link
